1aba0f4672d53c5cbcc514d364b3726dc975b4e3
“Ahmad” <a.sarwari@student.avans.nl>
2023-10-13 11:36:27 +0200

    Speed

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 int main()
 {
-   printf("Hello, World!");
-   printf("Hello, Galaxy!\0/\0/\0/"); //David
-   printf("Hello, Galaxy!"); //person 1
+   printf("Speed is constant");
    return 0;
 }
