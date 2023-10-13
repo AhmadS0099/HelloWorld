11bcc058c3d8eda723d8eea512c0a2e04a1cb8dc
David <dj.dorant@student.avans.nl>
2023-10-13 12:13:33 +0200

    qt

--- main.c
+++ main.c
@@ -5,5 +5,7 @@ int main()
    printf("Hello, Galaxy!\0/\0/\0/"); //David
    printf("Hello, Galaxy!"); //person 1
    printf("Hello, Universe!");//person 2
+
+   printf("Qt application feature");
    return 0;
 }
