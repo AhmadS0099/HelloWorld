a5ce0a19a13419d52a2a45d69e53f267ae2ff11d
Lucas <lph.robben@student.avans.nl>
2023-10-23 13:19:36 +0200

    added emergency stop

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main()
    printf("Hello, World!");
    printf("Hello, Galaxy!\0/\0/\0/"); //David
    printf("Hello, Galaxy!"); //person 1
+   printf("made it so elevator stops moving in emergency");//Lucas
    return 0;
 }
