3649406759d1bb6aa9333dac6c7ea5993ea2b7e8
Lucas <lph.robben@student.avans.nl>
2023-10-23 13:34:13 +0200

    revert back

--- main.c
+++ main.c
@@ -7,8 +7,5 @@ int main()
    printf("Hello, Galaxy!\0/\0/\0/"); //David
    printf("Hello, Galaxy!"); //person 1
    printf("made it so elevator stops moving in emergency");//Lucas
-   printf("Lift states added");
-   printf("Hello, Universe!");//person 2
-   printf("Qt application feature");
    return 0;
 }
